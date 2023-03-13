82b17c13b13abf88f9c1bcf035aa009848534c39
Godri5 <106228542+Godri5@users.noreply.github.com>
2023-03-13 09:37:55 +0300

    Remove unused endpoints from CreditProductController

--- src/main/java/org/aston/credit/controller/CreditProductController.java
+++ src/main/java/org/aston/credit/controller/CreditProductController.java
@@ -1,19 +1,12 @@
 package org.aston.credit.controller;
 
 import lombok.RequiredArgsConstructor;
-import org.aston.credit.dto.CreditProductRequestDto;
 import org.aston.credit.dto.CreditProductResponseDto;
 import org.aston.credit.entity.CreditProductEntity;
 import org.aston.credit.mapper.CreditProductMapper;
 import org.aston.credit.service.CreditProductService;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -28,44 +21,11 @@ public class CreditProductController {
 
     private final CreditProductMapper creditProductMapper;
 
-    @GetMapping("/{id}")
-    public ResponseEntity<?> getById(@PathVariable long id) {
-        try {
-            final CreditProductEntity creditProduct = creditProductService.getById(id);
-            final CreditProductResponseDto creditProductDto = creditProductMapper.toDto(creditProduct);
-            return new ResponseEntity<>(creditProductDto, HttpStatus.OK);
-        } catch (Exception EntityNotFoundException) {
-            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-        }
-    }
-
-    @PostMapping
-    public void create(@RequestBody CreditProductRequestDto creditProductRequestDto) {
-        final CreditProductEntity creditProduct = creditProductMapper.toEntity(creditProductRequestDto);
-        creditProductService.save(creditProduct);
-    }
-
-    @PutMapping
-    public void update(@RequestBody CreditProductRequestDto creditProductRequestDto) {
-        final CreditProductEntity creditProduct = creditProductMapper.toEntity(creditProductRequestDto);
-        creditProductService.save(creditProduct);
-    }
-
-    @DeleteMapping("/{id}")
-    public ResponseEntity<?> removeById(@PathVariable long id) {
-        try {
-            creditProductService.removeById(id);
-            return new ResponseEntity<>(HttpStatus.OK);
-        } catch (Exception EntityNotFoundException) {
-            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-        }
-    }
-
     /**
      * A-PROD.1 - Отправка информации об активных кредитных продуктах
      *
      * @return Список активных кредитных продуктов банка
-     * */
+     */
     @GetMapping
     public ResponseEntity<List<CreditProductResponseDto>> getAllActive() {
         List<CreditProductEntity> products = creditProductService.getAllActive();
