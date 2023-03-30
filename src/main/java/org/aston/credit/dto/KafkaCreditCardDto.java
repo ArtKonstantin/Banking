58460771e3f6422a9d151cc1e874038abcb74c85
Konstantin Artamonov <artkonstantin1987@gmail.com>
2023-03-30 11:36:19 +0300

    refactoring

--- src/main/java/org/aston/credit/dto/KafkaCreditCardDto.java
+++ src/main/java/org/aston/credit/dto/KafkaCreditCardDto.java
@@ -4,7 +4,7 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.aston.credit.entity.CardStatusEnum;
+import org.aston.credit.entity.enums.CardStatusEnum;
 
 @Data
 @Builder
