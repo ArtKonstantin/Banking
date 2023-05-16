ebcc1f9b3e012afbfecb2cfe8cd511bc2ae10bf0
Maxim <m.strybuk@astondevs.ru>
2023-05-16 18:54:26 +0300

    fix bug

--- src/main/java/org/aston/credit/dto/requests/CreditOrderRequestDto.java
+++ src/main/java/org/aston/credit/dto/requests/CreditOrderRequestDto.java
@@ -1,10 +1,7 @@
 package org.aston.credit.dto.requests;
 
 import io.swagger.v3.oas.annotations.media.Schema;
-import jakarta.validation.constraints.Min;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Pattern;
-import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.*;
 import lombok.Builder;
 import lombok.Value;
 import org.aston.credit.Constants;
@@ -17,26 +14,27 @@ import java.math.BigDecimal;
 public class CreditOrderRequestDto {
 
     @Schema(description = "id кредитного продукта", requiredMode = Schema.RequiredMode.REQUIRED)
+    @NotNull(message = Constants.FIELD_MUST_NOT_BE_BLANK)
     Long productId;
 
     @Schema(description = "сумма кредита", requiredMode = Schema.RequiredMode.REQUIRED)
     @Positive(message = Constants.FIELD_MUST_BE_POSITIVE)
-    @NotBlank(message = Constants.FIELD_MUST_NOT_BE_BLANK)
+    @NotNull(message = Constants.FIELD_MUST_NOT_BE_BLANK)
     BigDecimal amountRequested;
 
     @Schema(description = "срок кредита в месяцах", requiredMode = Schema.RequiredMode.REQUIRED)
     @Positive(message = Constants.FIELD_MUST_BE_POSITIVE)
-    @NotBlank(message = Constants.FIELD_MUST_NOT_BE_BLANK)
+    @NotNull(message = Constants.FIELD_MUST_NOT_BE_BLANK)
     Integer periodMonths;
 
     @Schema(description = "месячный доход клиента", requiredMode = Schema.RequiredMode.REQUIRED)
     @Min(value = 0, message = Constants.FIELD_MUST_NOT_BE_NEGATIVE)
-    @NotBlank(message = Constants.FIELD_MUST_NOT_BE_BLANK)
+    @NotNull(message = Constants.FIELD_MUST_NOT_BE_BLANK)
     BigDecimal monthlyIncome;
 
     @Schema(description = "ежемесячная долговая нагрузка клиента", requiredMode = Schema.RequiredMode.REQUIRED)
     @Min(value = 0, message = Constants.FIELD_MUST_NOT_BE_NEGATIVE)
-    @NotBlank(message = Constants.FIELD_MUST_NOT_BE_BLANK)
+    @NotNull(message = Constants.FIELD_MUST_NOT_BE_BLANK)
     BigDecimal monthlyExpenditure;
 
     @Schema(description = "ИНН клиента", requiredMode = Schema.RequiredMode.REQUIRED)
