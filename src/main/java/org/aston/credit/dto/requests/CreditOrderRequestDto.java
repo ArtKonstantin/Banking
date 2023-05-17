a49f46a1104c8873c820f6448becb2de99fa48dd
Maxim <m.strybuk@astondevs.ru>
2023-05-17 11:18:55 +0300

    fixed multiple import

--- src/main/java/org/aston/credit/dto/requests/CreditOrderRequestDto.java
+++ src/main/java/org/aston/credit/dto/requests/CreditOrderRequestDto.java
@@ -1,7 +1,11 @@
 package org.aston.credit.dto.requests;
 
 import io.swagger.v3.oas.annotations.media.Schema;
-import jakarta.validation.constraints.*;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Positive;
 import lombok.Builder;
 import lombok.Value;
 import org.aston.credit.Constants;
